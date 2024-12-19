5f1c6a3802a2ebae41804f4101b034b70dd821d2
Cas Limpens <2400410limpens@zuyd.nl>
2024-12-19 17:08:41 +0100

    Geen afbeeldingslink meer, teveel data.

--- WebApi1/Model/Drink.cs
+++ WebApi1/Model/Drink.cs
@@ -4,7 +4,6 @@ public class Drink
 {
 	private string _name;
 	private string _type;
-	private string _link;
 
 	public string Name
 	{
@@ -18,23 +17,10 @@ public class Drink
 		set { _type = value; }
 	}
 
-	public string Link
-	{
-		get { return _link; }
-		set { _link = value; }
-	}
-
-	public Drink(string name, string type, string link)
-	{
-		_name = name;
-		_type = type;
-		_link = link;
-	}
 	public Drink(string name, string type)
 	{
 		_name = name;
 		_type = type;
-		_link = "";
 	}
 }
 
